fff40864d8537e23c82821abe8659f7a018dd0e8
KIM, JINWOOK <zelonion@gmail.com>
2019-11-21 23:42:31 +0900

    Add source codes

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGithubAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
